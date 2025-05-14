7448b3efee4b08b289bd42842a85a4b056c700c5
Raghav Agrawal <codeworks2u@gmail.com>
2025-05-14 15:39:57 +0530

    Time: 22 ms (26.37%) | Memory: 47.2 MB (42.77%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int n;
+    const int MOD=1e9+7;
+    typedef unsigned long long ll;
+    vector<int>getNSL(vector<int>&arr){
+        stack<int>st;
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            if(st.empty()) ans[i]=-1;
+            else{
+                while(!st.empty() && arr[st.top()] >arr[i]) st.pop();
+                ans[i] = st.empty() ?-1:st.top();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int>getNSR(vector<int>&arr){
+        vector<int>ans(n);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()) ans[i]=n;
+            else{
+                while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+                ans[i] = st.empty()?n:st.top();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        n=arr.size();
+        vector<int>NSL=getNSL(arr);
+        vector<int>NSR=getNSR(arr);
+        ll ans=0;
+        for(int i=0;i<n;i++){
+            ll left = i-NSL[i];
+            ll right = NSR[i]-i;
+            ll combined = left*right;
+            ll total = combined*arr[i];
+            ans = (ans+total)%MOD;
+        }
+        return ans;
+    }
+};
