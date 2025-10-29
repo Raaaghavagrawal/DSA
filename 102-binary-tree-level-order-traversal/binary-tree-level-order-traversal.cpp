96ce55f004f99396cd8131856b8a4c12b5d919a9
Raghav Agrawal <codeworks2u@gmail.com>
2025-10-29 10:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (6.39%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -11,23 +11,16 @@
  */
 class Solution {
 public:
+    vector<vector<int>>ret;
+    void buildVector(TreeNode* root,int depth){
+        if(root==nullptr) return;
+        if(ret.size()==depth) ret.push_back(vector<int>());
+        ret[depth].push_back(root->val);
+        buildVector(root->left,depth+1);
+        buildVector(root->right,depth+1);
+    }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root==NULL) return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            vector<int>level;
-            for(int i=0;i<size;i++){
-                TreeNode* curr = q.front();
-                q.pop();
-                level.push_back(curr->val);
-                if(curr->left) q.push(curr->left);
-                if(curr->right) q.push(curr->right);
-            }
-            ans.push_back(level);
-        }
-        return ans;
+        buildVector(root,0);
+        return ret;
     }
 };
