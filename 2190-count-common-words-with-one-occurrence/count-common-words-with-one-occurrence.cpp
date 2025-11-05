a10f82f5579e51146759b7484930433b7db39fec
Raghav Agrawal <codeworks2u@gmail.com>
2025-11-05 15:06:14 +0530

    Time: 8 ms (81.24%) | Memory: 22.8 MB (45.78%) - LeetSync

---
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        unordered_map<string,int>freq1,freq2;
+        for(auto &w:words1) freq1[w]++;
+        for(auto &w:words2) freq2[w]++;
+        int count=0;
+        for(auto &p:freq1){
+            if(p.second==1 && freq2[p.first]==1) count++;
+        }
+        return count;
+    }
+};
