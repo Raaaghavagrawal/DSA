ceab005c05ffcc67b8072fc93348970622a62043
Raghav Agrawal <codeworks2u@gmail.com>
2026-01-04 13:55:09 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (72.65%) - LeetSync

---
+++ 994-prison-cells-after-n-days/prison-cells-after-n-days.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int n) {
+        if(n==0) return cells;
+        prison(cells,1);
+        prison(cells,(n-1)%14);
+        return cells;
+    }
+    void prison(vector<int>&cells,const int& N){
+        for(int i=0;i<N;i++){
+            vector<int>v(8,0);
+            for(int i=1;i<7;i++){
+                if(cells[i-1]==cells[i+1]) v[i]=1;
+                else v[i]=0;
+            }
+            cells=v;
+        }
+    }
+};
