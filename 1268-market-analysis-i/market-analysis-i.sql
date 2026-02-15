95f97adf0d2cbef9383a00730909c8fd0eb55758
Raghav Agrawal <codeworks2u@gmail.com>
2026-02-15 15:28:26 +0530

    Time: 1302 ms (30.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,3 @@
+select u.user_id as buyer_id, u.join_date, count(o.order_id) as orders_in_2019
+from users u left join orders o on o.buyer_id = user_id and year(order_date)='2019'
+group by u.user_id;
