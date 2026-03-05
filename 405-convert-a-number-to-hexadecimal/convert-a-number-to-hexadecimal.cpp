ac04358ae75de77ed8fdec03ed3cb37b1a0c3e04
Raghav Agrawal <codeworks2u@gmail.com>
2026-03-05 23:36:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (74.53%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -3,9 +3,9 @@ public:
     string toHex(int num) {
         unsigned int ournum=num;
         string str;
-        char arr[17]="0123456789abcdef";
+        char ourarray[17]="0123456789abcdef";
         do{
-            str+=arr[ournum%16];
+            str+=ourarray[ournum%16];
             ournum/=16;
         }while(ournum);
         return {str.rbegin(),str.rend()};
