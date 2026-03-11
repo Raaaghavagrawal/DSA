830966e853f839335f4350fda4ffd20b61b77b33
Raghav Agrawal <codeworks2u@gmail.com>
2026-03-11 21:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (2.35%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        int i=1;
-        int answer=0;
-        while(i*i <= n){
-            answer++;
-            i++;
-        }
-        return answer;
+        return sqrt(n);
     }
 };
