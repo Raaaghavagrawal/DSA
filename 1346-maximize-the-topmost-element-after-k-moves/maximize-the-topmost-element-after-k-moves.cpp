2b25de6986635ccc8d1e1ec6e7715f82c341285a
Raghav Agrawal <codeworks2u@gmail.com>
2026-04-03 18:26:04 +0530

    Time: 0 ms (100.00%) | Memory: 67.5 MB (58.39%) - LeetSync

---
+++ 1346-maximize-the-topmost-element-after-k-moves/maximize-the-topmost-element-after-k-moves.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumTop(vector<int>& nums, int k) {
+        if(k==0) return nums[0];
+        if(k%2!=0 && nums.size()==1) return -1;
+        int n=nums.size();
+        int i=0,maxi=0;
+        while(i<n && i<k-1){
+            maxi=max(maxi,nums[i]);
+            i++;
+        }
+        int ans=0;
+        if(k<n) ans=nums[k];
+        return max(maxi,ans);
+    }
+};
